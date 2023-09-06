2f7823526ee8b445c60e25003f9ec94cad7096ff
ssulphurr <ssulphurr@gmail.com>
2023-09-06 23:46:42 +0300

    adds logo link

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,9 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 import Navigation from "./Navigation";
 
 const SharedLayout = () => {
   return (
     <>
+      <NavLink to="/">
+        <a href="">
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/003/694/243/small/car-icon-in-flat-style-simple-traffic-icon-free-vector.jpg"
+            alt="go home"
+          />
+        </a>
+      </NavLink>
       <Navigation />
       <Outlet />
     </>
