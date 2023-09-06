c78abdf7697e37409b4ff50b49522e916c912f98
ssulphurr <ssulphurr@gmail.com>
2023-09-07 00:56:42 +0300

    adds car card draft

---
+++ src/components/CarCard.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const CarCard = () => {
+  return (
+    <>
+      <img src="" />
+      <p>car name</p>
+      <p>car price</p>
+      <p>car features</p>
+      <button>Learn more</button>
+    </>
+  );
+};
+
+export default CarCard;
