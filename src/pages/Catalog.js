b40f96a7b2302f87d7067666d5cebcad415ad832
ssulphurr <ssulphurr@gmail.com>
2023-09-09 19:35:21 +0300

    adds more styles to Catalog

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,22 +1,59 @@
 import { useEffect, useState } from "react";
+import { styled } from "styled-components";
 import CarCardsList from "../components/CarCardsList";
 import getAllCars from "../services/api";
+
+const CenterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const LoadMoreBtn = styled.button`
+  color: #3470ff;
+  font-family: Manrope;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px; /* 150% */
+  text-decoration-line: underline;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+const PStyled = styled.p`
+  font-family: Manrope;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+`;
 
 const Catalog = () => {
   const [allCars, setAllCars] = useState([]);
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showBtn, setShowBtn] = useState(true);
 
   useEffect(() => {
     const updateCars = async () => {
+      setIsLoading(true);
+
       const fetchedCars = await getAllCars(page, 8);
 
       setAllCars((prevState) => {
+        if (fetchedCars.length === 0) {
+          setIsLoading(false);
+          setShowBtn(false);
+        }
+
         if (!prevState) {
+          setIsLoading(false);
+
           return fetchedCars;
         }
 
         if (prevState.length > 0) {
-          console.log(prevState);
+          setIsLoading(false);
+
           return [...prevState, ...fetchedCars];
         }
       });
@@ -33,7 +70,15 @@ const Catalog = () => {
     <>
       <CarCardsList data={allCars} />
 
-      <button onClick={handlePagination}>Load more</button>
+      <CenterContainer>
+        {isLoading && <PStyled>Loading...</PStyled>}
+
+        {!isLoading && showBtn && (
+          <LoadMoreBtn onClick={handlePagination}>Load more</LoadMoreBtn>
+        )}
+
+        {!showBtn && <PStyled>There is no more car left</PStyled>}
+      </CenterContainer>
     </>
   );
 };
