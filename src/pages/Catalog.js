2edbe31cf93ea032a30cba5668c173d50e1defc8
ssulphurr <ssulphurr@gmail.com>
2023-09-10 10:56:34 +0300

    deletes commnets

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -27,34 +27,11 @@ const PStyled = styled.p`
   line-height: 24px;
 `;
 
-// const selectStyled = styled.select`
-//   display: flex;
-//   width: 160px;
-//   height: 48px;
-//   padding: 14px 41px 14px 24px;
-// `;
-
-// const ButtonStyled = styled.button`
-//   background-color: #3470ff;
-//   color: #fff;
-//   font-family: Manrope;
-//   font-size: 14px;
-//   font-weight: 600;
-//   line-height: 20px;
-//   width: 136px;
-//   height: 48px;
-//   border-radius: 12px;
-//   border: none;
-//   cursor: pointer;
-// `;
-
 const Catalog = () => {
   const [allCars, setAllCars] = useState([]);
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [showBtn, setShowBtn] = useState(true);
-  // const [model, setModel] = useState(null);
-  // const [filteredCars, setFilteredCars] = useState([]);
 
   useEffect(() => {
     const updateCars = async () => {
@@ -89,39 +66,8 @@ const Catalog = () => {
     setPage(page + 1);
   };
 
-  // const handleSelect = (e) => {
-  //   setModel(e.target.value);
-  // };
-
-  // useEffect(() => {
-  //   const filteredArr = allCars.filter((c) => {
-  //     if (c.model === model) {
-  //       return c;
-  //     }
-  //   });
-  //   setFilteredCars(filteredArr);
-  // }, [model, allCars]);
-
   return (
     <>
-      {/* <CenterContainer>
-        <div>
-          <PStyled>Car brand</PStyled>
-          <select name="brand" onClick={handleSelect}>
-            {allCars.map((car) => {
-              return <option value={car.model}>{car.model}</option>;
-            })}
-          </select>
-        </div>
-        <ButtonStyled>Search</ButtonStyled>
-      </CenterContainer>
-
-      {!model ? (
-        <CarCardsList data={allCars} />
-      ) : (
-        <CarCardsList data={filteredCars} />
-      )} */}
-
       <CarCardsList data={allCars} />
 
       <CenterContainer>
